9c66fecd9d372e8c0c4803310e3287b88f92ee4a
Egor1586 <egor115819@gmail.com>
2026-05-07 23:17:08 +0300

    ed

--- social_network/profile_app/models.py
+++ social_network/profile_app/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
+from user_app.models import User
+
 
 class Profile(models.Model):
-    user = models.OneToOneField("User", on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     birth_date = models.DateField(blank=True, null=True)
     
     signature = models.ImageField(upload_to='',blank=True, null=True )
     avatar = models.ImageField(upload_to='', blank=True, null=True)
     
     pseudonym = models.CharField(max_length=50)
-    friends = models.ManyToManyField("Profile", on_delete=models.CASCADE, related_name="friends")
+    friends = models.ManyToManyField("Profile")
     is_image_signature = models.BooleanField(default=False)
     is_text_signature = models.BooleanField(default=True)
 
 
 class FriendsRequest(models.Model):
-    from_profile = models.OneToOneField('Profile', on_delete=models.CASCADE, related_name="from_profile")
-    to_profile = models.OneToOneField('Profile', on_delete=models.CASCADE, related_name="to_profile")
+    from_profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name="sent_requests")
+    to_profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name="received_requests")
     create_at = models.DateTimeField(auto_now_add=True)
     
